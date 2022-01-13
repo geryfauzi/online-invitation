6bc323658bb12ce42bb5bd74b8bccb11000a08f2
Gery Akbar Fauzi <geryakbarfauzi@gmail.com>
2022-01-13 14:04:33 +0700

    - Fix upload ke gallery

--- upload.js
+++ upload.js
@@ -3,13 +3,16 @@ const uploadImage = async (req,res) => {
     try{
         const {file} = req.files;
         file.mv(`${__dirname}/public/uploads/${file.name}`, function (error){
-            if(error)
-                res.json({code : 0, message : error});
+            if(error) {
+                console.log(error)
+                res.json({code: 0, message: error});
+            }
             else{
                 res.json({code : 1, message : "Sukses upload file", path : `${file.name}`});
             }
         });
     } catch(err){
+        console.log(err)
         res.json({code : 0, message : err});
     }
 }
