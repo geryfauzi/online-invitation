6f6948b14ab759ead3407e754c9e6f93449ec0af
Gery Akbar Fauzi <geryakbarfauzi@gmail.com>
2022-01-18 09:47:32 +0700

    - Revisi backend

--- handler/tamu.js
+++ handler/tamu.js
@@ -5,7 +5,7 @@ const path = require('path')
 
 function generateCode() {
     let text = "";
-    let possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let possible = "0123456789";
     for (let i = 0; i <= 5; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
     return text;
@@ -15,7 +15,7 @@ const getWeddingGuest = async (req, res) => {
     let connect = DB.config
     let id = req.params.id
     try {
-        connect.query("SELECT tamu.id, tamu.id_grup, grup.nama AS nama_grup, tamu.nama, tamu.telepon, tamu.jumlah,tamu.rsvp, tamu.kehadiran, tamu.id_pernikahan, tamu.kode, tamu.is_vip, tamu.is_family, tamu.ucapan, COUNT(detail_sesi.id_sesi) AS jumlah_sesi FROM tamu LEFT JOIN detail_sesi ON tamu.id = detail_sesi.id_tamu LEFT JOIN grup ON tamu.id_grup = grup.id WHERE tamu.id_pernikahan = ? GROUP BY tamu.id ORDER BY tamu.id DESC", [id], (error, result) => {
+        connect.query("SELECT tamu.id, tamu.id_grup, grup.nama AS nama_grup, tamu.nama, tamu.telepon, tamu.jumlah_dewasa, tamu.jumlah_anak, tamu.rsvp, tamu.kehadiran, tamu.id_pernikahan, tamu.kode, tamu.is_vip, tamu.is_family, tamu.ucapan, COUNT(detail_sesi.id_sesi) AS jumlah_sesi FROM tamu LEFT JOIN detail_sesi ON tamu.id = detail_sesi.id_tamu LEFT JOIN grup ON tamu.id_grup = grup.id WHERE tamu.id_pernikahan = ? GROUP BY tamu.id ORDER BY tamu.id DESC", [id], (error, result) => {
             console.log(result)
             return res.json({
                 data: result
@@ -30,7 +30,7 @@ const insertGuest = async (req, res) => {
     let connect = DB.config
     let data = req.body
     try {
-        connect.query("INSERT INTO tamu(nama,telepon,jumlah,id_pernikahan,id_grup,kode,is_vip,is_family) VALUES(?,?,?,?,?,?,?,?)", [data.nama, data.telepon, data.jumlah, data.id_pernikahan, data.id_grup, data.kode, data.is_vip, data.is_family], (error, result) => {
+        connect.query("INSERT INTO tamu(nama,telepon,jumlah_dewasa,jumlah_anak,id_pernikahan,id_grup,kode,is_vip,is_family) VALUES(?,?,?,?,?,?,?,?,?)", [data.nama, data.telepon, data.jumlah_dewasa, data.jumlah_anak, data.id_pernikahan, data.id_grup, data.kode, data.is_vip, data.is_family], (error, result) => {
             if (!error) {
                 data.sesi.forEach(item => {
                     connect.query("INSERT INTO detail_sesi VALUES(?,?)", [item, result.insertId], (error1, result1) => {
@@ -68,15 +68,16 @@ const insertFromExcel = async (req, res) => {
         excel(filePath).then((rows) => {
             rows.shift()
             rows.forEach((item) => {
-                connect.query("INSERT INTO tamu(nama,telepon,jumlah,id_grup,id_pernikahan,kode,is_vip,is_family) VALUES(?,?,?,?,?,?,?,?)", [
+                connect.query("INSERT INTO tamu(nama,telepon,jumlah_dewasa,jumlah_anak,id_grup,id_pernikahan,kode,is_vip,is_family) VALUES(?,?,?,?,?,?,?,?,?)", [
                     item[0],
                     item[1],
                     item[2],
                     item[3],
+                    item[4],
                     data.id_pernikahan,
                     generateCode(),
-                    item[4],
-                    item[5]
+                    item[5],
+                    item[6]
                 ], (error, result) => {
                     if (!error) {
                         //Insert data sesi
@@ -113,7 +114,7 @@ const updateGuest = async (req, res) => {
     let connect = DB.config
     let data = req.body
     try {
-        connect.query("UPDATE tamu SET nama = ?, telepon = ?, jumlah = ?, id_grup = ?, is_family = ?, is_vip = ? WHERE id = ?", [data.nama, data.telepon, data.jumlah, data.id_grup, data.is_family, data.is_vip, data.id], (error, result) => {
+        connect.query("UPDATE tamu SET nama = ?, telepon = ?, jumlah_dewasa = ?, jumlah_anak = ?, id_grup = ?, is_family = ?, is_vip = ? WHERE id = ?", [data.nama, data.telepon, data.jumlah_dewasa, data.jumlah_anak, data.id_grup, data.is_family, data.is_vip, data.id], (error, result) => {
             if (!error) {
                 data.sesi.forEach(item => {
                     connect.query("DELETE FROM detail_sesi WHERE id_sesi = ? AND id_tamu = ?", [item, data.id], (error1, result1) => {
@@ -146,7 +147,7 @@ const getGuestBook = async (req, res) => {
     let connect = DB.config
     let id = req.params.id
     try {
-        connect.query("SELECT tamu.id, tamu.id_grup, grup.nama AS nama_grup, tamu.nama, tamu.telepon, tamu.jumlah,tamu.rsvp, tamu.kehadiran, tamu.id_pernikahan, tamu.kode, tamu.is_vip, tamu.is_family FROM tamu LEFT JOIN detail_sesi ON tamu.id = detail_sesi.id_tamu LEFT JOIN grup ON tamu.id_grup = grup.id WHERE tamu.id_pernikahan = ? AND tamu.kehadiran != 'Belum Dikonfirmasi' GROUP BY tamu.id ORDER BY tamu.id DESC", [id], (error, result) => {
+        connect.query("SELECT tamu.id, tamu.id_grup, grup.nama AS nama_grup, tamu.nama, tamu.telepon, tamu.jumlah_dewasa, tamu.jumlah_anak,tamu.rsvp, tamu.kehadiran, tamu.id_pernikahan, tamu.kode, tamu.is_vip, tamu.is_family, tamu.waktu_checkin, tamu.waktu_checkout FROM tamu LEFT JOIN detail_sesi ON tamu.id = detail_sesi.id_tamu LEFT JOIN grup ON tamu.id_grup = grup.id WHERE tamu.id_pernikahan = ? AND tamu.kehadiran != 'Belum Dikonfirmasi' GROUP BY tamu.id ORDER BY tamu.id DESC", [id], (error, result) => {
             return res.json({
                 data: result
             })
